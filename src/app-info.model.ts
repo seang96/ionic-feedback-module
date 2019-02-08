0e1bad5c06c9d105f92ea6a582d30700d4d75fcb
Sean Greenawalt <sean-greenawalt@alumni.psu.edu>
2019-02-08 13:46:11 -0500

    Added Ionic Pro Configuration to AppInfo

--- src/app-info.model.ts
+++ src/app-info.model.ts
@@ -1,6 +1,9 @@
+import { Pro } from '@ionic/pro';
+
 export class AppInfo {
 	public appName: string;
 	public packageName: string;
 	public versionCode: string | number;
 	public versionNumber: string;
+	public ionicProConfiguration = Pro.deploy.getConfiguration();
 }
